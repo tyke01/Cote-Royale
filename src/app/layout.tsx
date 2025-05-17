19095f241836b87172e988d6c192b50d1ac0b61f
tyke01 <techvictor653@gmail.com>
2025-05-17 13:35:31 +0300

    update: add font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,20 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Raleway } from "next/font/google";
+import localFont from "next/font/local";
+
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const raleway = Raleway({
+  variable: "--font-raleway",
   subsets: ["latin"],
+  display: "swap",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+// Font files can be colocated inside of `app`
+const gambarino = localFont({
+  variable: "--font-gambarino",
+  src: "./gambarino.woff2",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -23,12 +28,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="en" className={`${raleway.variable} ${gambarino.variable} antialiased`}>
+      <body>{children}</body>
     </html>
   );
 }
